89f249697e0431e8c8eeb0269df861205ba70279
EC2 Default User <ec2-user@ip-172-31-27-178.ec2.internal>
2021-12-03 10:26:20 +0000

    fix 7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    
+
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = 'You have created book successfully.'
@@ -19,9 +19,17 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
-    @user = current_user
+    # @books = Book.all　投稿したものを表示する。
+    # @books = Book.all.sort {|a,b| b.favorites.count <=> a.favorites.count} いいね数の順番に投稿を表示。
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort {|a,b|
+        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        a.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }
     @book = Book.new
+    @user=current_user
   end
 
   def show
@@ -47,7 +55,7 @@ class BooksController < ApplicationController
     else
        render :edit
     end
-    
+
   end
 
   def destroy
