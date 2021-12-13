8e48218eadeb2d018d29b5d76b933cbdecb95ca5
EC2 Default User <ec2-user@ip-172-31-27-178.ec2.internal>
2021-12-13 17:15:40 +0000

    9C

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: "0xt1303c726164u@ezweb.ne.jp"
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   get '/search', to: 'searches#search'
   resources :groups, only: [:index, :new, :create, :show, :edit, :update] do
     resource :group_users, only: [:create, :destroy]
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
 
 end
